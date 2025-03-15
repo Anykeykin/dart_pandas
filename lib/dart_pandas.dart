0b0203721e53ebe66d2c1c4e068dd2e4fe220d27
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-03-15 12:13:40 +0300

    ref

--- lib/dart_pandas.dart
+++ lib/dart_pandas.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+
 library;
 
 export 'src/dart_pandas_base.dart';
 export 'src/data_frame.dart';
-// TODO: Export any libraries intended for clients of this package.
