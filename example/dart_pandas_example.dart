6be5824b89778b6e38ef313238e2cc8a7c933a2f
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-03-15 12:17:17 +0300

    upd example for data frame

--- example/dart_pandas_example.dart
+++ example/dart_pandas_example.dart
@@ -1,3 +1,26 @@
+import 'package:dart_pandas/dart_pandas.dart';
 
 void main() {
+  // Create DataFrame from CSV
+  final df = DataFrame.fromCsv('Your csv path.csv');
+  df.printDataFrame();
+
+  // Filtration data
+  final filteredDf = df.filter('age', (age) => age > 30);
+  filteredDf.printDataFrame();
+
+  // Sort data
+  final sortedDf = df.sort('age', ascending: false);
+  sortedDf.printDataFrame();
+
+  // Data arithmetic
+  print('Sum of salary: ${df.sum('salary')}');
+  print('Mean of salary: ${df.mean('salary')}');
+
+  // Group data
+  final groupedDf = df.groupBy('department');
+  groupedDf.forEach((key, value) {
+    print('Department: $key');
+    value.printDataFrame();
+  });
 }
